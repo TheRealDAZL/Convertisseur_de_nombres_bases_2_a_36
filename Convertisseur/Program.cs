7228338b53f373c9c64598b4535ff5d3504ad2dc
TheRealDAZL <0667750@cegepoutaouais.qc.ca>
2023-11-19 12:38:58 -0500

    Version améliorée de mon programme, mais pas encore vraiment optimisée

--- Convertisseur/Program.cs
+++ Convertisseur/Program.cs
@@ -1,6 +1,6 @@
-﻿// Titre : convertir un nombre entier, de valeur nulle ou positive représenté en base 10 en sa représentation dans une base entière entre 2 et 36 inclusivement
+﻿// Titre : convertir un nombre entier représenté en base 10 en sa représentation dans une base entière entre 2 et 36 inclusivement
 
-// Déclarer et initialiser les variables
+// Déclarer et initialiser les constantes et la variable
 string[] SYMBOLES_POUR_CONVERSION = new string[36] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
 string continuer = "";
 
@@ -8,9 +8,9 @@ string continuer = "";
 do
 {
     // Déclarer puis initialiser ou réinitialiser les variables
-    string representation_finale = "";
     int representation_initiale = -1;
     int base_de_lexponentielle = -1;
+    bool ok = true;
 
     // Exécuter le bloc de code suivant
     do
@@ -19,7 +19,7 @@ do
         try
         {
             // Entrer le nombre à convertir
-            Console.WriteLine("Entrez un nombre entier, de valeur nulle ou positive, tel que représenté en base 10 :");
+            Console.WriteLine("Entrez un nombre entier tel que représenté en base 10 :");
 
             // Lire la valeur entrée
             representation_initiale = int.Parse(Console.ReadLine());
@@ -31,17 +31,20 @@ do
         // S'il y a une ou des erreurs de format, afficher ces messages
         catch (FormatException)
         {
+            ok = false;
             Console.WriteLine("Cette entrée n'est pas valide.\n");
         }
 
         // S'il y a une ou des erreurs d'overflow, afficher ces messages
         catch (System.OverflowException)
         {
+            ok = false;
+
             // Référence : Microsoft - Double.MaxValue Field : https://learn.microsoft.com/en-us/dotnet/api/system.double.maxvalue?view=net-7.0
-            Console.WriteLine("Ce nombre est trop gros. Entrez une valeur plus petite ou égale à 1,7976931348623157 * 10^308, et plus grande ou égale à 0.\n");
+            Console.WriteLine("Ce nombre est trop large. Entrez une valeur plus petite.\n");
         }
     }
-    while (representation_initiale < 0);
+    while (!ok);
 
     // Exécuter le bloc de code suivant
     do
@@ -75,7 +78,7 @@ do
     while (base_de_lexponentielle < 2 || base_de_lexponentielle > 36);
 
     // Exécuter la méthode CalculerEtAfficher
-    CalculerEtAfficher(representation_initiale, representation_finale, base_de_lexponentielle);
+    CalculerEtAfficher(representation_initiale, base_de_lexponentielle);
 
     // Afficher le message
     Console.WriteLine("\nVoulez-vous continuer? Tapez \"n\" (pour non), puis appuyez sur la touche Entrée pour terminer le programme.");
@@ -92,28 +95,48 @@ do
 
 
 
-void CalculerEtAfficher(int representation_initiale, string representation_finale, int base_de_lexponentielle)
+void CalculerEtAfficher(int representation_initiale, int base_de_lexponentielle)
 {
-    // Déclarer les variables
+    // Déclarer et/ou initialiser les variables
     int plus_gros_exposant;
     int restant;
     int chiffre_a_convertir;
     int position_du_chiffre;
+    string representation_finale = "";
 
     // Si le nombre entré correspond aux conditions données, alors on exécute le bloc de code suivant
-    if (representation_initiale > 0)
+    if (representation_initiale != 0)
     {
-        // Trouver le plus gros exposant pour décomposer la valeur entrée dans la base donnée
-        // Référence : Microsoft - Math.Log Method https://learn.microsoft.com/en-us/dotnet/api/system.math.log2?view=net-7.0
-        plus_gros_exposant = (int)Math.Floor(Math.Log(representation_initiale, base_de_lexponentielle));
+        if (representation_initiale > 0)
+        {
+            // Trouver le plus gros exposant pour décomposer la valeur entrée dans la base donnée
+            // Référence : Microsoft - Math.Log Method https://learn.microsoft.com/en-us/dotnet/api/system.math.log2?view=net-7.0
+            plus_gros_exposant = (int)Math.Floor(Math.Log(representation_initiale, base_de_lexponentielle));
+        }
+
+        else
+        {
+            // Trouver le plus gros exposant pour décomposer la valeur entrée dans la base donnée
+            // Référence : Microsoft - Math.Log Method https://learn.microsoft.com/en-us/dotnet/api/system.math.log2?view=net-7.0
+            plus_gros_exposant = (int)Math.Floor(Math.Log(-representation_initiale, base_de_lexponentielle));
+        }
 
         // Afficher une partie du message
         // Référence : GeeksforGeeks - Program to Print a New Line in C# https://www.geeksforgeeks.org/program-to-print-a-new-line-in-c-sharp/
         Console.WriteLine("\nLe nombre " + representation_initiale + " représenté en base 10 se décompose en la somme d'exponentielle(s) en base " + base_de_lexponentielle + " de la façon suivante :");
 
-        // Initialiser la variable restant
-        restant = representation_initiale;
+        if (representation_initiale > 0)
+        {
+            // Initialiser la variable restant
+            restant = representation_initiale;
+        }
 
+        else
+        {
+            // Initialiser la variable restant
+            restant = -representation_initiale;
+        }
+        
         // Pour i compris entre le plus_gros_exposant et 0, exécuter le bloc de code suivant
         // Référence : W3Schools - C# For Loop https://www.w3schools.com/cs/cs_for_loop.php
         for (int compteur_dexposants = plus_gros_exposant; compteur_dexposants >= 0; compteur_dexposants--)
@@ -127,8 +150,17 @@ void CalculerEtAfficher(int representation_initiale, string representation_final
             // Définir ou redéfinir la variable position_du_chiffre
             position_du_chiffre = compteur_dexposants + 1;
 
-            // Afficher le résultat
-            Console.WriteLine("Chiffre " + position_du_chiffre + " -> " + SYMBOLES_POUR_CONVERSION[chiffre_a_convertir]);
+            if (representation_initiale > 0)
+            {
+                // Afficher le résultat
+                Console.WriteLine("Chiffre " + position_du_chiffre + " -> (+1) * " + SYMBOLES_POUR_CONVERSION[chiffre_a_convertir]);
+            }
+
+            else
+            {
+                // Afficher le résultat
+                Console.WriteLine("Chiffre " + position_du_chiffre + " -> (-1) * " + SYMBOLES_POUR_CONVERSION[chiffre_a_convertir]);
+            }
 
             // Si i > 0, alors afficher ce message
             if (compteur_dexposants > 0)
@@ -141,8 +173,17 @@ void CalculerEtAfficher(int representation_initiale, string representation_final
             representation_finale = representation_finale + SYMBOLES_POUR_CONVERSION[chiffre_a_convertir];
         }
 
-        // Afficher la variable representation_finale
-        Console.WriteLine("\nLe nombre " + representation_initiale + " représenté en base 10 est équivalent au nombre " + representation_finale + " représenté en base " + base_de_lexponentielle + ".");
+        if (representation_initiale > 0)
+        {
+            // Afficher la variable representation_finale
+            Console.WriteLine("\nLe nombre " + representation_initiale + " représenté en base 10 est équivalent au nombre " + representation_finale + " représenté en base " + base_de_lexponentielle + ".");
+        }
+
+        else
+        {
+            // Afficher la variable representation_finale
+            Console.WriteLine("\nLe nombre " + representation_initiale + " représenté en base 10 est équivalent au nombre -" + representation_finale + " représenté en base " + base_de_lexponentielle + ".");
+        }
     }
 
     else
