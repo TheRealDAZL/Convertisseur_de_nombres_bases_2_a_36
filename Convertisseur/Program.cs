f4df2ff0de129db875fd569b690934398734fe81
TheRealDAZL <0667750@cegepoutaouais.qc.ca>
2023-12-06 21:57:07 -0500

    Version fonctionnelle

--- Convertisseur/Program.cs
+++ Convertisseur/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Titre : convertir un nombre entier représenté en base 10 en sa représentation dans une base entière entre 2 et 36 inclusivement
 
+
+
 // Déclarer et initialiser les constantes et la variable
 string[] SYMBOLES_POUR_CONVERSION = new string[36] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
 string continuer = "";
